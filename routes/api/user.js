dfa91086c6fe5ca282ec1f6d4a3ef9d08ce3266d
OleksiiChernii <oleksii.chernii@gmail.com>
2023-05-11 21:47:34 +0300

    first-commit

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,16 +1,36 @@
-const express = require('express');
-const {register, login, logout, current} = require('../../models/user');
-const validate = require('../../validation/auth');
-const validateToken = require('../../middleware/auth')
-
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const {
+  register,
+  login,
+  logout,
+  current,
+  avatar,
+} = require("../../models/user");
+const validate = require("../../validation/auth");
+const validateToken = require("../../middleware/auth");
 const router = express.Router();
 
-router.post('/register', validate(), register);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../..", "tmp"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-router.post('/login', validate(), login);
+const upload = multer({ storage });
 
-router.post('/logout', validateToken, logout)
+router.post("/register", validate(), register);
 
-router.get('/current', validateToken, current)
+router.post("/login", validate(), login);
+
+router.post("/logout", validateToken, logout);
+
+router.get("/current", validateToken, current);
+
+router.patch("/avatars", validateToken, upload.single("avatar"), avatar);
 
 module.exports = router;
